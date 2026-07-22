398deada1cdc10b421847ffd42f8e6862db3ab31
Noob <noobdunet@gmail.com>
2026-07-22 21:05:57 +0200

    v1
    APT only, KDE and Gnome

---
+++ update.sh
@@ -0,0 +1,14 @@
+#///////////////////
+# update
+#///////////////////
+
+sudo apt upadate -y
+sudo apt upagrade -y
+sudo apt full-upgrade -y
+sudo apt autoremove -y
+flatpak refresh -y
+
+#///////////////////
+# reboot
+#///////////////////
+sudo reboot
